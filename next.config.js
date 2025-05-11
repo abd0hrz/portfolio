a1061a46b957fc66b9c145637bce9b72d55b1e6e
Abd0hrz <b.harizi@esi-sba.dz>
2025-05-11 12:08:30 +0200

    Updateed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   images: {
     domains: [
       'media.graphassets.com',
@@ -10,6 +13,8 @@ const nextConfig = {
       'media2.dev.to',
     ],
   },
+  basePath: isGithubPages ? '/portfolio' : '',
+  assetPrefix: isGithubPages ? '/portfolio/' : '',
 };
 
 module.exports = nextConfig;
